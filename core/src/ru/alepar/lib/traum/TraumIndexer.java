6d5fcb07b0f588322feb8f2ec0c4008be8cc45b1
alepar <alepar@gmail.com>
2011-04-17 01:13:21 +0400

    something's off with the wildcard queries this is not a real commit, just wanted to share with you :sigh:

--- core/src/ru/alepar/lib/traum/TraumIndexer.java
+++ core/src/ru/alepar/lib/traum/TraumIndexer.java
@@ -1,13 +1,11 @@
 package ru.alepar.lib.traum;
 
 import org.apache.lucene.store.RAMDirectory;
-import ru.alepar.lib.index.AuthorIndex;
-import ru.alepar.lib.index.BookIndex;
-import ru.alepar.lib.index.LuceneAuthorIndex;
-import ru.alepar.lib.index.LuceneBookIndex;
+import ru.alepar.lib.index.*;
 
 import java.io.File;
 import java.util.Date;
+import java.util.Set;
 
 public class TraumIndexer {
 
@@ -37,16 +35,18 @@ public class TraumIndexer {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Date start = new Date();
         BookIndex bookIndex = new LuceneBookIndex(new RAMDirectory());
         AuthorIndex authorIndex = new LuceneAuthorIndex(new RAMDirectory());
-        Iterable<String> feeder = new FileFeeder(new File("f:\\test"));
+        Iterable<String> feeder = new FileFeeder(new File("f:\\traum"));
         TraumIndexer indexer = new TraumIndexer(feeder, bookIndex, authorIndex, new TraumBookInfoExtractor());
         indexer.go();
         Date end = new Date();
         System.out.println((end.getTime() - start.getTime()) / 1000);
         System.out.println("Query time");
+        Set<Author> authors = authorIndex.find("фр*");
+        System.out.println(authors);
     }
 
 }

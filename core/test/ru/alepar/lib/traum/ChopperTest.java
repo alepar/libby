e3f74bbbdffc92c6464102eb956cbf8ff9b1c2de
alepar <alepar@gmail.com>
2011-04-21 10:15:43 +0400

    fixing unit tests for *nix

--- core/test/ru/alepar/lib/traum/ChopperTest.java
+++ core/test/ru/alepar/lib/traum/ChopperTest.java
@@ -11,29 +11,42 @@ public class ChopperTest {
 
     @Test
     public void worksfineWithRootAsABaseDir() throws Exception {
-        Chopper chopper = new Chopper(new File("\\"));
+        Chopper chopper = new Chopper(new File(concat("","")));
 
-        assertThat(chopper.chop(new File("\\ru\\alepar\\smth")), equalTo("ru\\alepar\\smth"));
+        assertThat(chopper.chop(new File(concat("", "ru", "alepar", "smth"))), equalTo(concat("ru", "alepar", "smth")));
     }
 
     @Test
     public void worksfineWithSubfolderAsABaseDir() throws Exception {
-        Chopper chopper = new Chopper(new File("\\ru"));
+        Chopper chopper = new Chopper(new File("", "ru"));
 
-        assertThat(chopper.chop(new File("\\ru\\alepar\\smth")), equalTo("alepar\\smth"));
+        assertThat(chopper.chop(new File(concat("", "ru", "alepar", "smth"))), equalTo(concat("alepar", "smth")));
     }
 
     @Test
     public void someOtherWeirdCaseIsFineToo() throws Exception {
-        Chopper chopper = new Chopper(new File("\\test"));
+        Chopper chopper = new Chopper(new File("", "test"));
 
-        assertThat(chopper.chop(new File("\\test\\ru\\Г\\Громов Борис\\Громов - Лубянская ласточка.fb2.zip")), equalTo("ru\\Г\\Громов Борис\\Громов - Лубянская ласточка.fb2.zip"));
+        assertThat(chopper.chop(new File(concat("", "test", "ru", "Г", "Громов Борис", "Громов - Лубянская ласточка.fb2.zip"))), equalTo(concat("ru", "Г", "Громов Борис", "Громов - Лубянская ласточка.fb2.zip")));
     }
 
     @Test
     public void chopsBasePathToDot() throws Exception {
-        Chopper chopper = new Chopper(new File("\\test"));
+        Chopper chopper = new Chopper(new File("", "test"));
 
-        assertThat(chopper.chop(new File("\\test")), equalTo("."));
+        assertThat(chopper.chop(new File("", "test")), equalTo("."));
     }
+
+    private static String concat(String... names) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < names.length; i++) {
+            String name = names[i];
+            if(i > 0) {
+                result.append(File.separatorChar);
+            }
+            result.append(name);
+        }
+        return result.toString();
+    }
+
 }
